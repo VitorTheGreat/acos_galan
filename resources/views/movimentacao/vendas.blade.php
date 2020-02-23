73f3a80c41e1d8474d07119893e5e42ec559df23
vitor hugo <vitor.services@gmail.com>
2020-02-23 09:35:20 -0300

    creating fields of movimentations screen

--- resources/views/movimentacao/vendas.blade.php
+++ resources/views/movimentacao/vendas.blade.php
@@ -15,7 +15,59 @@
                     </div>
                     <div class="card-body">
                       <div class="table-responsive">
-
+                        <form>
+                          <h5>Vendedor</h5>
+                            <div class="form-row">
+                              <div class="col">
+                                <input type="text" class="form-control" value="" placeholder="Vendedor" name="">
+                              </div>
+                            </div>
+                            <br>
+                            <h5>Dados Cliente</h5>
+                            <div class="form-row">
+                              <div class="col">
+                                <input type="text" class="form-control" placeholder="Nome">
+                              </div>
+                              <div class="col">
+                                <input type="text" class="form-control" placeholder="Endereço">
+                              </div>
+                              <div class="col">
+                                <input type="text" class="form-control" placeholder="Telefone">
+                              </div>
+                              <div class="col">
+                                <input type="text" class="form-control" placeholder="celular">
+                              </div>
+                            </div>
+                            <br>
+                            <h5>Produto(s)</h5>
+                            <div class="form-row">
+                                <div class="col">
+                                  <input type="text" class="form-control" placeholder="ID Produto (barcode)">
+                                </div>
+                                <div class="col">
+                                  <input type="text" class="form-control" placeholder="Descrição">
+                                </div>
+                                <div class="col">
+                                  <input type="text" class="form-control" placeholder="QTD">
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="Preço do produto" disabled>
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="Total" disabled>
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="Peso">
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="Valor/KG" disabled>
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="Total" disabled>
+                                </div>
+                            </div>
+                            <button type="button" class="btn btn-success">Inserir Produto</button>
+                          </form>
                       </div>
                     </div>
                   </div>
