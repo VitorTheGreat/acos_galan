872fadd263db5b656a0cce0b4271528110401c23
Vitor Hugo <vitor.services@gmail.com>
2020-04-21 21:27:59 -0300

    created simple tables with their relationships

---
+++ database/migrations/2020_04_21_230913_create_supplier_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSupplierTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supplier', function (Blueprint $table) {
+            $table->bigIncrements('id')->unique();
+            $table->string('razao_social', 250)->unique();
+            $table->string('email', 50)->unique();
+            $table->string('cnpj', 18)->unique();
+            $table->string('ie', 15)->unique();
+            $table->string('telefone', 13)->nullable();
+            $table->unsignedBigInteger('states_id');
+            $table->foreign('states_id')
+                  ->references('id')->on('states')
+                  ->constrained();
+            $table->string('endereco', 50);
+            $table->string('bairro', 30);
+            $table->string('cidade', 30);
+            $table->string('cep', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supplier');
+    }
+}
